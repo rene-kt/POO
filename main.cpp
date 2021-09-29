64a2dcccdad7f706d959fbfd9bd90aec0f6986a4
Rene <renejr.arraes286@gmail.com>
2021-09-29 17:32:48 -0300

    finished MELHOR CASO

--- main.cpp
+++ main.cpp
@@ -43,9 +43,66 @@ using namespace std;
        
     }
 
+    // PROCURAR_MENOR method
+     int returnSmallestValue(int arr[], int arr_size){
+        
+        int smallestValue = arr[0];
+
+        for(int i = 1; i<arr_size; i++){
+            if(arr[i] < smallestValue)  smallestValue = arr[i];
+            
+        }
+        return smallestValue;
+    }
+
+    // PROCURAR_MAIOR method
+    int returnGreatestValue(int arr[], int arr_size){
+        int greatestValue = arr[0];
+
+        for(int i = 1; i<arr_size; i++){
+            if(arr[i] > greatestValue)  greatestValue = arr[i];
+            
+        }
+        return greatestValue;
+    }
+
+    // PROCURAR_MENOR_POS method
+     int returnPositionOfTheSmallestValue(int arr[], int arr_size){
+        
+        int smallestValue = returnSmallestValue(arr, arr_size);
+
+        for(int i =0; i<arr_size; i++){
+            if(arr[i] == smallestValue) return i;
+        }
+        return -1;
+    }
+    // PROCURAR_MENOR_POS_APOS method
+    int returnPositionOfTheSmallestValueAfterGivenValue(int arr[], int arr_size, int position){
+        
+        int smallestValue = arr[position + 1]; 
+
+        for(int i = position + 1; i<arr_size; i++){
+            if(arr[i] < smallestValue) smallestValue = arr[i];
+        }
+
+        return returnPositionOfTheValue(arr, arr_size, smallestValue);
+    }
+
+    // PROCURAR_MELHOR_SE method
+    int returnPositionOfTheSmallestValueOfAMan(int arr[], int arr_size){
+        int smallestValue = arr[0];
+
+        for(int i =0; i<arr_size; i++){
+            if(arr[i] > 0 && arr[i] < smallestValue) smallestValue = arr[i];
+        }
+        return returnPositionOfTheValue(arr, arr_size, smallestValue);
+    }
+
+
+
 
 int main() {
-    int array[7] = {50, -19, 2, 5, 17, 50, 50};
-    std::cout << returnPositionOfTheValueAfterGivenValue(array, 6, 50, 0) << '\n';
+    int array[7] = {50, -19, -22, 56, 2, 50, 5};
+    std::cout << returnPositionOfTheSmallestValueOfAMan(array, 7) << '\n';
     return 0;
 }
