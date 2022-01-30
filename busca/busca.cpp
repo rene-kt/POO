f6a057e3352304c618a891f9bb6173892d696b35
Renê Júnior <renejr.arraes286@gmail.com>
2022-01-30 17:42:07 -0300

    finish busca

--- busca/busca.cpp
+++ busca/busca.cpp
@@ -2,118 +2,135 @@
 #include <sstream>
 #include <list>
 
-
-class Fone {
+class Phone
+{
 private:
     std::string id;
     std::string number;
 
 public:
-    Fone(std::string id, std::string number) {
+    Phone(std::string id, std::string number)
+    {
         this->id = id;
         this->number = number;
     }
 
-    static bool isValid(std::string number) {
-        for(int i : number) {
-            if( i == '(' || i == ')' || (i >= '0' && i <= '9') ) {
+    static bool isValid(std::string number)
+    {
+        for (int i : number)
+        {
+            if (i == '(' || i == ')' || (i >= '0' && i <= '9'))
+            {
                 continue;
             }
-            else {
+            else
+            {
                 return false;
             }
         }
         return true;
     }
 
-    std::string getId() {
+    std::string getId()
+    {
         return this->id;
     }
 
-    std::string getNumber() {
+    std::string getNumber()
+    {
         return this->number;
     }
 
-    void setId(std::string id) {
-        if(isValid(id)) {
-            this->id = id;
-        }
-        else {
-            std::cout << "Invalid ID" << '\n';
-        }
+    void setId(std::string id)
+    {
+        this->id = id;
     }
 
-    void setNumber(std::string number) {
+    void setNumber(std::string number)
+    {
         this->number = number;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, Fone& fone) {
-        os << fone.id << ':' << fone.number;
+    friend std::ostream &operator<<(std::ostream &os, Phone &phone)
+    {
+        os << phone.id << ":" << phone.number;
         return os;
-}
+    }
 };
 
-class Contact {
+class Contact
+{
 private:
     std::string name;
-    std::list<Fone> fones;
+    std::list<Phone> phones;
 
 public:
-    Contact(std::string name) {
+    Contact(std::string name)
+    {
         this->name = name;
     }
 
-    void addFone(Fone fone) {
-        this->fones.push_back(fone);
+    void addPhone(Phone phone)
+    {
+        this->phones.push_back(phone);
     }
 
-    void rmFone(int index) {
-        if(fones.size() == 0) {
-            std::cout << "Não há fones para remover" << '\n';
+    void rmPhone(int index)
+    {
+        if (phones.size() == 0)
+        {
+            std::cout << "The list is empty" << '\n';
         }
-        else {
-            std::list<Fone>::iterator it = this->fones.begin();
+        else
+        {
+            std::list<Phone>::iterator it = this->phones.begin();
             std::advance(it, index);
-            this->fones.erase(it);
+            this->phones.erase(it);
         }
     }
 
-    std::string getName() {
+    std::string getName()
+    {
         return this->name;
     }
 
-    std::list<Fone> getFones() {
-        return this->fones;
+    std::list<Phone> getPhones()
+    {
+        return this->phones;
     }
 
-    void setName(std::string name) {
+    void setName(std::string name)
+    {
         this->name = name;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, Contact& contact) {
+    friend std::ostream &operator<<(std::ostream &os, Contact &contact)
+    {
         os << contact.name << ' ';
         int cont{0};
-        for(Fone fone : contact.fones) {
+        for (Phone phone : contact.phones)
+        {
             os << '[' << cont << ':';
-            os << fone;
+            os << phone;
             os << ']';
         }
         return os;
     }
-
-
 };
 
-class Agenda {
+class Agenda
+{
 private:
     std::list<Contact> contacts;
 
 public:
-
-    int findPosByName(std::string name) {
+    int findByName(std::string name)
+    {
         int pos = 0;
-        for(Contact c: contacts) {
-            if(c.getName() == name) {
+        for (Contact c : contacts)
+        {
+            if (c.getName() == name)
+            {
                 return pos;
             }
             pos++;
@@ -123,129 +140,135 @@ public:
 
     Agenda() {}
 
-    Contact findContact(std::string name) {
-        int pos = findPosByName(name);
-        if(pos == -1) {
-            std::cout << "Não há contato com esse nome" << '\n';
+    Contact findContact(std::string name)
+    {
+        int pos = findByName(name);
+        if (pos == -1)
+        {
+            std::cout << "The name was not found" << '\n';
             return Contact("");
         }
-        else {
+        else
+        {
             auto it = this->contacts.begin();
             std::advance(it, pos);
             return *it;
         }
-        
     }
 
-    void addContact(Contact contact) {
+    void addContact(Contact contact)
+    {
         this->contacts.push_back(contact);
     }
 
-    void rmContact(std::string name) {
-        int pos = findPosByName(name);
-        if(pos == -1) {
-            std::cout << "Não há contato com esse nome" << '\n';
+    void rmContact(std::string name)
+    {
+        int pos = findByName(name);
+        if (pos == -1)
+        {
+            std::cout << "The name was not found" << '\n';
         }
-        else {
+        else
+        {
             auto it = this->contacts.begin();
             std::advance(it, pos);
             this->contacts.erase(it);
         }
     }
 
-    friend std::ostream& operator<<(std::ostream& os, Agenda& agenda) {
-        for(Contact c : agenda.contacts) {
+    friend std::ostream &operator<<(std::ostream &os, Agenda &agenda)
+    {
+        for (Contact c : agenda.contacts)
+        {
             os << c << '\n';
         }
         return os;
     }
-
-
-
 };
 
-
-int main() {
+int main()
+{
     Agenda agenda;
 
-    while(true) {
+    while (true)
+    {
         std::string line;
         std::getline(std::cin, line);
         std::stringstream ss(line);
         std::string command;
         ss >> command;
 
-        if(command == "add") {
+        if (command == "add")
+        {
             std::string name;
             std::string id;
             std::string number;
             ss >> name;
-            std::string format;
-            ss >> format;
             Contact c(name);
-            
-            for(int i = 0; i < (int) format.size(); i++) {
-                if(format[i] == ':') {
-                    format[i] = ' ';
+            ss >> id;
+            ss >> number;
+            if (Phone::isValid(number))
+            {
+                if (agenda.findByName(name) != -1)
+                {
+                    c.addPhone(Phone(id, number));
+                }
+                else
+                {
+                    c.setName(name);
+                    Phone phone(id, number);
+                    c.addPhone(phone);
+                    agenda.addContact(c);
                 }
             }
-            std::stringstream ss2(format);
-            ss2 >> id >> number;
-            if(Fone::isValid(number)) {
-                //se o contato já existir, adiciona o fone
-                if(agenda.findPosByName(name) != -1) {
-                    c.addFone(Fone(id, number));
-                } else {
-                c.setName(name);
-                Fone fone(id, number);
-                c.addFone(fone);
-                agenda.addContact(c);
-
-                }
-                
-            }
-            else {
+            else
+            {
                 std::cout << "Invalid number" << '\n';
             }
-            
-
         }
-        else if(command == "rm") {
+        else if (command == "rm")
+        {
             std::string name;
             ss >> name;
-            int pos = agenda.findPosByName(name);
-            if(pos == -1) {
-                std::cout << "Não há contato com esse nome" << '\n';
+            int pos = agenda.findByName(name);
+            if (pos == -1)
+            {
+                std::cout << "Name was not found" << '\n';
             }
-            else {
+            else
+            {
                 agenda.rmContact(name);
             }
         }
-        else if(command == "find") {
+        else if (command == "find")
+        {
             std::string name;
             ss >> name;
             Contact c = agenda.findContact(name);
-            if(c.getName() == "") {
-                std::cout << "Não há contato com esse nome" << '\n';
+            if (c.getName() == "")
+            {
+                std::cout << "Name was not found" << '\n';
             }
-            else {
+            else
+            {
                 std::cout << c << '\n';
             }
         }
 
-        else if(command == "exit") {
+        else if (command == "exit")
+        {
             break;
         }
 
-        else if(command == "show") {
+        else if (command == "show")
+        {
             std::cout << agenda << '\n';
         }
 
-        else {
+        else
+        {
             std::cout << "Invalid command" << '\n';
         }
-
     }
     return 0;
-
 }
